e53c5c47ff94c293cf0bc4df30bc1ce97941454a
Ilias Bilionis <ebilionis@gmail.com>
2014-01-19 18:13:49 -0600

    Added a unittest.

---
+++ tests/test_GPyModel.py
@@ -0,0 +1,23 @@
+"""
+Unit tests for the GPyModel class.
+"""
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.split(__file__)[0]))
+import GPy
+import pymcmc
+
+
+if __name__ == '__main__':
+    model = GPy.examples.regression.olympic_marathon_men(optimize=False, plot=False)
+    mcmc_model = pymcmc.GPyModel(model)
+    print str(mcmc_model)
+    print mcmc_model.log_likelihood
+    print mcmc_model.log_prior
+    print mcmc_model.num_params
+    print mcmc_model.params
+    mcmc_model.params = mcmc_model.params
+    print mcmc_model.param_names
+    print mcmc_model.grad_log_likelihood
+    print mcmc_model.grad_log_prior
