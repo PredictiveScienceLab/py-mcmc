ce77c83c5e283c68b0c666804abef648ee8612ac
Ilias Bilionis <ebilionis@gmail.com>
2014-01-19 18:13:02 -0600

    I had forgotten to add _GPymodel.py

---
+++ pymcmc/_GPyModel.py
@@ -0,0 +1,78 @@
+"""
+A generic wrapper for a GPy model.
+
+Author:
+    Ilias Bilionis
+"""
+
+from . import Model
+import numpy as np
+
+
+__all__ = ['GPyModel']
+
+
+class GPyModel(Model):
+    
+    """
+    A generic wrapper for GPy model.
+
+    :param model:   A GPy model.
+    :type model:    GPy.Model
+    :param name:    A name for the model.
+    :type name:     str
+    """
+
+    def __init__(self, model, name='GPy model wrapper'):
+        """
+        Initialize the object.
+        """
+        self.model = model
+        super(GPyModel, self).__init__(name=name)
+
+    def __getstate__(self):
+        return self.model.getstate()
+
+    def __setstate__(self, state):
+        self.setstate(state)
+
+    @property
+    def log_likelihood(self):
+        return self.model.log_likelihood()
+
+    @property
+    def log_prior(self):
+        return self.model.log_prior()
+
+    @property
+    def num_params(self):
+        return self.model.num_params_transformed()
+
+    @property
+    def params(self):
+        return self.model._get_params_transformed()
+
+    @params.setter
+    def params(self, value):
+        self.model._set_params_transformed(value)
+
+    @property
+    def param_names(self):
+        return self.model._get_param_names()
+
+    @property
+    def grad_log_likelihood(self):
+        g = self.model._log_likelihood_gradients()
+        return self.model._transform_gradients(g)
+
+    @property
+    def grad_log_prior(self):
+        g = self.model._log_prior_gradients()
+        if isinstance(g, float):
+            g = np.array([g] * self.num_params)
+        return self.model._transform_gradients(g)
+
+    def __str__(self):
+        s = super(GPyModel, self).__str__()
+        s += '\n' + str(self.model)
+        return s
