fe02a6e9da3534797ebedeec011daf11368237a9
Ilias Bilionis <ebilionis@gmail.com>
2014-01-22 09:27:24 -0600

    Continuing db.

--- pymcmc/_databse.py
+++ pymcmc/_databse.py
@@ -7,18 +7,29 @@ Author:
 
 import tables as pt
 import numpy as np
+import os
+
+
+CHAIN_TYPE = {'id': pt.UInt16Col(),
+              'date': pt.StringCol(itemsize=16),
 
 
 class DataBase(object):
 
     """
     A database to store MCMC chains.
-    """
 
+    :param filename:     The filename of the database.
+    :type filename:      str
+    """
 
     def __init__(self, filename):
         """
         Initialize the object.
         """
         self.filename = filename
-        self.db = pt.open_file(filename, mode='a')
+        if os.path.exists(filename) and pt.isPyTablesFile(filename):
+            self.db = pt.open_file(filename, mode='a')
+        else:
+            self.db = pt.open_file(filename, mode='w')
+            fd.create_group('/', 'mcmc', 'Metropolis-Hastings Algorithm Data')
