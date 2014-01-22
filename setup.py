f508e9dc9be15534bce5105cafcbffea6628fc28
Ilias Bilionis <ebilonis@gmail.com>
2014-01-22 14:14:11 -0600

    Added a setup script.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG example
+"""
+
+from distutils.core import setup, Extension
+
+setup (name = 'pymcmc',
+       version = '0.1',
+       author      = "Ilias Bilionis",
+       description = """Simple MCMC Python module""",
+       packages = ['pymcmc']
+       )
