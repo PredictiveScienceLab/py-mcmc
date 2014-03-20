f8d75e191c487823cac448ec51d63a49b4216153
Ilias Bilionis <ebilonis@gmail.com>
2014-03-20 14:06:25 -0500

    Added several priors from GPy.

--- pymcmc/_priors.py
+++ pymcmc/_priors.py
@@ -6,19 +6,42 @@ Author:
 """
 
 
-__all__ = ['UninformativeScalePrior']
+__all__ = ['Prior', 'GaussianPrior', 'LogGaussianPrior',
+           'MultivariateGaussianPrior', 'GammaPrior',
+           'InverseGammaPrior', 'UninformativeScalePrior']
 
 
-import GPy
+import GPy.core.domains as domains
+import GPy.core.priors as priors
+Prior = priors.Prior
+GaussianPrior = priors.Gaussian
+LogGaussianPrior = priors.LogGaussian
+MultivariateGaussianPrior = priors.MultivariateGaussian
+GammaPrior = priors.Gamma
+InverseGammaPrior = priors.inverse_gamma
 import numpy as np
 
 
-class UninformativeScalePrior(GPy.core.priors.Prior):
+class UninformativeScalePrior(Prior):
 
-    domain = GPy.core.domains.POSITIVE
+    """
+    An uninformative prior.
+    """
+
+    domain = domains.POSITIVE
 
     def lnpdf(self, x):
+        """
+        :param x:   The value of the parameter.
+        :type x:    :class:`numpy.ndarray`
+        :returns:   The logarithm of the probability.
+        """
         return -np.log(x)
 
     def lnpdf_grad(self, x):
+        """
+        :param x:   The value of the parameter.
+        :type x:    :class:`numpy.ndarray`
+        :returns:   The gradient of the logarithm of the probability.
+        """
         return -1. / x
