Fixed wrong sign in MALA.

pymcmc/_mala_proposal.py
@@ -42,13 +42,13 @@ class MALAProposal(GradProposal, SingleParameterTunableProposalConcept):
         SingleParameterTunableProposalConcept.__init__(self, **kwargs)
 
     def _sample(self, old_params, old_grad_params):
-        return (old_params -
+        return (old_params +
                 0.5 * self.dt ** 2 * old_grad_params +
                 self.dt * np.random.randn(old_params.shape[0]))
 
     def __call__(self, new_params, old_params, old_grad_params):
         return np.sum(norm.logpdf(new_params,
-                                  old_params -
+                                  old_params +
                                   0.5 * self.dt ** 2 * old_grad_params,
                                   self.dt ** 2))
 