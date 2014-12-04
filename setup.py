163f0bc2d4a5fa502337d5329535c0327a1517aa
Ilias Bilionis <ebilionis@gmail.com>
2014-12-04 13:40:12 -0500

    Prepared setup.py for pypi.'

--- setup.py
+++ setup.py
@@ -7,8 +7,24 @@ setup.py file for SWIG example
 from distutils.core import setup, Extension
 
 setup (name = 'pymcmc',
-       version = '0.6',
-       author      = "Ilias Bilionis",
-       description = """Simple MCMC Python module""",
-       packages = ['pymcmc']
+       version = '0.0a1',
+       author      = 'Ilias Bilionis',
+       author_email = 'ibilion@purdue.edu',
+       license = 'LGPL',
+       description = 'A python module implementing some generic MCMC routines',
+       long_description = """\
+The main purpose of this module is to serve as a simple MCMC framework for
+generic models. Probably the most useful contribution at the moment, is that
+it can be used to train Gaussian process (GP) models implemented in the 
+[GPy package](http://sheffieldml.github.io/GPy/).
+""",
+       packages = ['pymcmc'],
+       classifiers = ['Development Status :: 3 - Alpha',
+                      'Intended Audience :: Science/Research',
+                      'Topic :: Scientific/Engineering :: Mathematics'
+                      'Programming Language :: Python :: 2',
+                      'Programming Language :: Python :: 2.6',
+                      'Programming Language :: Python :: 2.7'],
+        keywords = 'Markov-Chain-Monte-Carlo MCMC Metrpolis-Adjusted-Langevin-Dynamics MALA GPy',
+        install_requires = ['GPy>=0.6.0']
        )
